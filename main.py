aca8e4ea57d403d76a81af7aeb04db4a8fb91822
Jett Thompson <jett.thompson.59@gmail.com>
2023-04-22 12:50:15 +1000

    make the code more general with an array of input values and an array of variables

--- main.py
+++ main.py
@@ -4,34 +4,41 @@ def get_mean_squared_error(expected, actual):
     return tensorflow.reduce_mean(tensorflow.square(expected - actual))
 
 class MyModel:
-    def __init__(self, matrix):
-        self.matrix = matrix
-    
+    def __init__(self):
+        self.matrix = tensorflow.Variable([[1., 1.], [1., 1.]])
+
+    def variables(self):
+        return [self.matrix]
+
     def calculate(self, input):
         return tensorflow.matmul(input, self.matrix)
     
-    @tensorflow.function
     def loss(self, input, expected):
         return get_mean_squared_error(expected, self.calculate(input))
 
+@tensorflow.function
+def combined_loss(model, input_values, expected_values):
+    result = 0
+    for (input, expected) in zip(tensorflow.unstack(input_values), tensorflow.unstack(expected_values)):
+        result += model.loss(input, expected)
+    return result
 
-matrix = tensorflow.Variable([[1.0, 2.0], [3.0, 4.0]])
-model = MyModel(matrix)
-input = tensorflow.constant([[8.0, 9.0]])
-expected = tensorflow.constant([[10.0, 11.0]])
-initial_loss = model.loss(input, expected)
+model = MyModel()
+variables = model.variables()
+input_values = tensorflow.constant([[[8.0, 9.0]]])
+expected_values = tensorflow.constant([[[10.0, 11.0]]])
+initial_loss = combined_loss(model, input_values, expected_values)
 print("Initial loss: %s" % initial_loss.numpy())
 
 learning_rate = 0.01
 for i in range(10):
     with tensorflow.GradientTape() as tape:
-        loss = model.loss(input, expected)
-        gradient = tape.gradient(loss, model.matrix)
-        matrix.assign_sub(gradient * learning_rate)
-        loss = model.loss(input, expected)
+        loss = combined_loss(model, input_values, expected_values)
+        gradients = tape.gradient(loss, variables)
+        for (gradient, variable) in zip(gradients, variables):
+            variable.assign_sub(gradient * learning_rate)
+        loss = combined_loss(model, input_values, expected_values)
         print("Loss at step %d: %s" % (i, loss.numpy()))
 
-loss = model.loss(input, expected)
+loss = combined_loss(model, input_values, expected_values)
 print("Final loss: %s" % loss.numpy())
-print("matrix: %s" % matrix.numpy())
-
